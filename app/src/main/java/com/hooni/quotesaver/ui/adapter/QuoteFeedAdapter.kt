a2fc9b577b85104befc40aa03f68bc44fa7254ec
Hoon Jung <hoon.michael.jung@gmail.com>
2020-12-10 20:44:40 +0900

    Add showing random backgrounds to quote

--- app/src/main/java/com/hooni/quotesaver/ui/adapter/QuoteFeedAdapter.kt
+++ app/src/main/java/com/hooni/quotesaver/ui/adapter/QuoteFeedAdapter.kt
@@ -2,11 +2,15 @@ package com.hooni.quotesaver.ui.adapter
 
 import android.content.Intent
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import coil.ImageLoader
+import coil.request.ImageRequest
 import com.hooni.quotesaver.R
 import com.hooni.quotesaver.data.model.Quote
 import com.hooni.quotesaver.databinding.ListItemQuoteBinding
+import com.hooni.quotesaver.util.backgroundImages
 
 class QuoteFeedAdapter(private val quotes: List<Quote>, private val favoriteQuotes: List<Quote>, private val favoriteClickListener: (Quote) -> Unit) :
     RecyclerView.Adapter<QuoteFeedAdapter.QuoteViewHolder>() {
@@ -26,6 +30,7 @@ class QuoteFeedAdapter(private val quotes: List<Quote>, private val favoriteQuot
             binding.imageViewListItemQuoteShare.setOnClickListener {
                 shareQuote(quote)
             }
+            setBackgroundImage(binding.root)
 
             if (favoriteQuotes.contains(quote)) binding.imageViewListItemQuoteLiked.setImageResource(R.drawable.ic_favorite)
             else binding.imageViewListItemQuoteLiked.setImageResource(R.drawable.ic_favorite_border)
@@ -41,6 +46,20 @@ class QuoteFeedAdapter(private val quotes: List<Quote>, private val favoriteQuot
 
             val shareIntent = Intent.createChooser(intentContent, "Share your Quote!")
             binding.root.context.startActivity(shareIntent)
+        }
+
+        private fun setBackgroundImage(view: View) {
+            val background = backgroundImages.random()
+            val request = ImageRequest.Builder(binding.root.context)
+                .data(background)
+                .target { drawable ->
+                    view.background = drawable
+                }
+                .build()
+            val imageLoader = ImageLoader.Builder(binding.root.context)
+                .crossfade(true)
+                .build()
+            imageLoader.enqueue(request)
         }
 
         private fun addToFavorites(quote: Quote, likeClickListener: (Quote) -> Unit, favoriteQuotes: List<Quote>) {
--- app/src/main/java/com/hooni/quotesaver/util/Constants.kt
+++ app/src/main/java/com/hooni/quotesaver/util/Constants.kt
@@ -1,3 +1,29 @@
 package com.hooni.quotesaver.util
 
+import com.hooni.quotesaver.R
+
 const val NUMBER_OF_QUOTES_RETURNED_AT_ONCE = 30
+
+val backgroundImages = listOf(
+    R.drawable.background_1,
+    R.drawable.background_2,
+    R.drawable.background_3,
+    R.drawable.background_4,
+    R.drawable.background_5,
+    R.drawable.background_6,
+    R.drawable.background_7,
+    R.drawable.background_8,
+    R.drawable.background_9,
+    R.drawable.background_10,
+    R.drawable.background_11,
+    R.drawable.background_12,
+    R.drawable.background_13,
+    R.drawable.background_14,
+    R.drawable.background_15,
+    R.drawable.background_16,
+    R.drawable.background_17,
+    R.drawable.background_18,
+    R.drawable.background_19,
+    R.drawable.background_20,
+
+)
