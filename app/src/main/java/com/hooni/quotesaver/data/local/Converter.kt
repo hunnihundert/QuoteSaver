4004c8774f758124d49d7d6448622a570a69ae91
Hoon Jung <hoon.michael.jung@gmail.com>
2020-12-19 11:10:50 +0900

    Fix minor issues

--- app/src/main/java/com/hooni/quotesaver/data/local/Converter.kt
+++ app/src/main/java/com/hooni/quotesaver/data/local/Converter.kt
@@ -1,6 +1,5 @@
 package com.hooni.quotesaver.data.local
 
-import android.util.Log
 import androidx.room.TypeConverter
 
 class Converter {
@@ -10,19 +9,14 @@ class Converter {
 
     @TypeConverter
     fun convertTagsListToString(tagList: List<String>): String {
-        Log.d(TAG, "convertTagsListToString: StringList: $tagList")
         var tagListString = ""
         for(tag in tagList) tagListString += "$tag,"
         tagListString = tagListString.removeSuffix(",")
-        Log.d(TAG, "convertTagsListToString: String: $tagListString")
         return tagListString
     }
 
     @TypeConverter
     fun convertStringToTagsList(tagListString: String): List<String> {
-        Log.d(TAG, "convertStringToTagsList: String: $tagListString")
-        val listOfTags = tagListString.split(",").map{it.trim()}
-        Log.d(TAG, "convertStringToTagsList: StringList: ${listOfTags}")
-        return listOfTags
+        return tagListString.split(",").map{it.trim()}
     }
 }
