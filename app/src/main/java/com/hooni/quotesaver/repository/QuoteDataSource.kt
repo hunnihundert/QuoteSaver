10815cfbf8fa59ec78dc474b2aea42591eb96df4
Hoon Jung <hoon.michael.jung@gmail.com>
2021-01-06 19:27:44 +0900

    Implement Paging 3

---
+++ app/src/main/java/com/hooni/quotesaver/repository/QuoteDataSource.kt
@@ -0,0 +1,41 @@
+package com.hooni.quotesaver.repository
+
+import android.util.Log
+import androidx.paging.PagingSource
+import com.hooni.quotesaver.data.model.Quote
+import com.hooni.quotesaver.data.remote.QuotesApi
+import com.hooni.quotesaver.util.NUMBER_OF_QUOTES_RETURNED_AT_ONCE
+import retrofit2.HttpException
+import java.io.IOException
+
+private const val INITIAL_OFFSET = 0
+
+class QuoteDataSource(private val quoteApi: QuotesApi, private val category: String) :
+    PagingSource<Int, Quote>() {
+
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Quote> {
+        val offset = params.key ?: INITIAL_OFFSET
+
+        return try {
+            val apiResult =
+                quoteApi.getQuotesByCategory(category, NUMBER_OF_QUOTES_RETURNED_AT_ONCE, offset)
+            Log.d("tag", "load: next: ${apiResult.next} / previous: ${apiResult.previous}")
+            val nextKey = getKey(apiResult.next)
+            val prevKey = getKey(apiResult.previous)
+            LoadResult.Page(apiResult.results, prevKey, nextKey)
+        } catch (exception: IOException) {
+            LoadResult.Error(exception)
+        } catch (exception: HttpException) {
+            LoadResult.Error(exception)
+        }
+    }
+
+    private fun getKey(url: String?): Int? {
+        var key: Int? = null
+        url?.let {
+            key = it.substringAfterLast("offset=").substringBeforeLast("&tags").toIntOrNull()
+        }
+        return key
+    }
+
+}
--- app/src/main/java/com/hooni/quotesaver/ui/viewmodel/FeedViewModel.kt
+++ app/src/main/java/com/hooni/quotesaver/ui/viewmodel/FeedViewModel.kt
@@ -1,24 +1,28 @@
 package com.hooni.quotesaver.ui.viewmodel
 
-import android.os.Bundle
-import android.os.Parcelable
+import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewModelScope
-import com.hooni.quotesaver.data.model.ApiQuoteResult
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
+import androidx.paging.map
 import com.hooni.quotesaver.data.model.ApiTagResult
 import com.hooni.quotesaver.data.model.Quote
-import com.hooni.quotesaver.data.remote.Resource
-import com.hooni.quotesaver.data.remote.Status.*
 import com.hooni.quotesaver.repository.QuoteRepository
 import com.hooni.quotesaver.util.getRandomImage
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import retrofit2.HttpException
+import java.net.SocketTimeoutException
 
 class FeedViewModel(private val quoteRepository: QuoteRepository) : ViewModel() {
-
 
     sealed class Progress {
         object Idle : Progress()
@@ -28,115 +32,69 @@ class FeedViewModel(private val quoteRepository: QuoteRepository) : ViewModel() 
 
     internal val progress = MutableLiveData<Progress>(Progress.Idle)
 
-    internal val quoteResultWithImages = MutableLiveData<ApiQuoteResult>()
     internal val favoriteQuotes = quoteRepository.getAllFavorites()
         .onStart {
-            progress.value = Progress.Loading }
+            progress.value = Progress.Loading
+        }
         .onEach {
-            progress.value = Progress.Idle }
+            progress.value = Progress.Idle
+        }
         .asLiveData()
 
-    val currentSearchTerm = MutableLiveData("")
-    internal var lastRequestedSearch = ""
-
-    private var isNewRequest = true
-
-    private val nextItems = MutableLiveData<String?>()
-    private val previousItems = MutableLiveData<String?>()
+    var currentSearchTerm: String? = null
+    private var currentSearchResult: Flow<PagingData<Quote>>? = null
 
     private lateinit var fullScreenQuote: Quote
 
-    internal var feedRecyclerViewStateBundle: Bundle? = null
-    internal var feedRecyclerViewState: Parcelable? = null
-
-    internal fun loadRandomQuotes() {
-        viewModelScope.launch {
-            val tags = getTags()
-            when (tags.status) {
-                LOADING -> {
-                    progress.value = Progress.Loading
-                }
-                ERROR -> {
-                    progress.value = Progress.Error(tags.message ?: "Unknown Error")
-                }
-                SUCCESS -> {
-                    progress.value = Progress.Idle
-                    setRandomCategory(tags.data!!.results!!)
-                }
-            }
-        }
-    }
-
-    private suspend fun getTags(): Resource<ApiTagResult> {
-        return quoteRepository.getTags()
-    }
-
-    private fun setRandomCategory(categories: List<ApiTagResult.QuoteTags>) {
-        val randomCategory = categories.random().name
-        currentSearchTerm.value = randomCategory
-        lastRequestedSearch = currentSearchTerm.value!!
-        getQuotesByCategory()
-    }
-
-    private fun getQuotesByCategory() {
-        viewModelScope.launch {
-            progress.value = Progress.Loading
-            val apiResult =
-                quoteRepository.getQuotesByCategory(lastRequestedSearch, nextItems.value)
-            when (apiResult.status) {
-                LOADING -> {
-                    progress.value = Progress.Loading
-                }
-                ERROR -> {
-                    progress.value = Progress.Error(apiResult.message ?: "Unknown Error")
-                }
-                SUCCESS -> {
-                    progress.value = Progress.Idle
-                    if(isNewRequest) quoteResultWithImages.value = addImageToQuotes(apiResult)
-                    else quoteResultWithImages.value = addNewApiResultToCurrent(apiResult)
-
+    private suspend fun getRandomCategory(): String {
+        progress.value = Progress.Loading
+        var tags: ApiTagResult? = null
+        withContext(Dispatchers.IO) {
+            try {
+                tags = quoteRepository.getTags()
+            } catch (exception: Exception) {
+                when (exception) {
+                    is HttpException -> {
+                        progress.postValue(Progress.Error(exception.message()))
+                    }
+                    is SocketTimeoutException -> {
+                        progress.postValue(Progress.Error(exception.message ?: "Socket Time Out"))
+                    }
+                    else -> {
+                        progress.postValue(Progress.Error(exception.message ?: "Unknown Error"))
+                    }
                 }
             }
         }
+        return tags?.results?.random()?.name ?: ""
     }
 
-    private fun addImageToQuotes(apiResult: Resource<ApiQuoteResult>): ApiQuoteResult {
-        setNextPreviousItems(apiResult)
-        return createQuotesWithImages(apiResult)
-    }
-
-    private fun setNextPreviousItems(apiQueryResponse: Resource<ApiQuoteResult>) {
-        nextItems.value = apiQueryResponse.data!!.next
-        previousItems.value = apiQueryResponse.data.previous
-    }
-
-    private fun createQuotesWithImages(apiQueryResponse: Resource<ApiQuoteResult>): ApiQuoteResult {
-        val quotesWithImages = mutableListOf<Quote>()
-        apiQueryResponse.data!!.results.forEach { quote ->
-            quotesWithImages.add(
-                Quote(
-                    quote.quote,
-                    quote.author,
-                    quote.likes,
-                    quote.tags,
-                    quote.pk,
-                    getRandomImage().toString(),
-                    quote.language
-                )
-            )
+    fun getQuotesByCategory(query: String): Flow<PagingData<Quote>> {
+        Log.d(TAG, "getQuotesByCategory: currentSearchResult: $currentSearchResult / currentSearch: $currentSearchTerm / query: $query")
+        val lastResult = currentSearchResult
+        if (query == currentSearchTerm && lastResult != null) {
+            return lastResult
         }
-        return ApiQuoteResult(
-            apiQueryResponse.data.next,
-            apiQueryResponse.data.previous,
-            quotesWithImages)
+        currentSearchTerm = query
+        val newResult = quoteRepository.getQuotesByCategory(query).map { pagingData ->
+            pagingData.map {
+                createQuotesWithImages(it)
+            }
+        }.cachedIn(viewModelScope)
+        currentSearchResult = newResult
+        return newResult
     }
 
-    private fun addNewApiResultToCurrent(apiResult: Resource<ApiQuoteResult>): ApiQuoteResult {
-        return ApiQuoteResult(
-            quoteResultWithImages.value!!.next,
-            quoteResultWithImages.value!!.previous,
-            quoteResultWithImages.value!!.results
-                    + addImageToQuotes(apiResult).results)
+    private fun createQuotesWithImages(quote: Quote): Quote {
+        return Quote(
+            quote.quote,
+            quote.author,
+            quote.likes,
+            quote.tags,
+            quote.pk,
+            getRandomImage().toString(),
+            quote.language
+        )
     }
 
     internal fun addToFavorites(quote: Quote) {
@@ -151,38 +109,20 @@ class FeedViewModel(private val quoteRepository: QuoteRepository) : ViewModel() 
         }
     }
 
-    internal fun startNewRequest() {
-        if (currentSearchTerm.value.isNullOrBlank()) {
-            // empty search
-        } else {
-            resetRequestParameters()
-            lastRequestedSearch = currentSearchTerm.value!!
-            getQuotesByCategory()
-        }
-    }
-
-    private fun resetRequestParameters() {
-        isNewRequest = true
-        nextItems.value = ""
-        previousItems.value = ""
-    }
-
-    internal fun addNewItems() {
-        if (nextItems.value != null) getQuotesByCategory()
-    }
-
-    internal fun resetIsNewRequest() {
-        isNewRequest = false
-    }
-
-    internal fun getIsNewRequest() = isNewRequest
-
-    internal fun setFullscreenQuote(quote: Quote){
+    internal fun setFullscreenQuote(quote: Quote) {
         fullScreenQuote = quote
     }
 
     internal fun getFullscreenQuote(): Quote {
         return fullScreenQuote
+    }
+
+    internal suspend fun setRandomCategoryAsSearchTerm() {
+        currentSearchTerm = getRandomCategory()
+    }
+
+    companion object {
+        private const val TAG = "FeedViewModel"
     }
 
 }
