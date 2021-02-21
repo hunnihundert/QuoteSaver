e998df664e9b14e597ba41c33f90b1cf629d2b09
Hoon Jung <hoon.michael.jung@gmail.com>
2021-02-22 08:53:15 +0900

    Add fragment transaction slide in/out

--- app/src/main/java/com/hooni/quotesaver/MainActivity.kt
+++ app/src/main/java/com/hooni/quotesaver/MainActivity.kt
@@ -1,10 +1,11 @@
 package com.hooni.quotesaver
 
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
-import androidx.navigation.NavHost
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
@@ -15,8 +16,6 @@ import com.hooni.quotesaver.ui.view.FeedFragmentDirections
 class MainActivity : AppCompatActivity() {
 
     private lateinit var bottomNavigationView: BottomNavigationView
-    private lateinit var navHostFragment: NavHostFragment
-    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,9 +25,23 @@ class MainActivity : AppCompatActivity() {
 
     private fun initBottomNavigation() {
         bottomNavigationView = findViewById(R.id.bottom_navigation)
-        navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        navController = navHostFragment.navController
-        bottomNavigationView.setupWithNavController(navController)
-
+        bottomNavigationView.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.feedFragment_menu -> {
+                    findNavController(R.id.nav_host_fragment).navigate(
+                        FavoritesFragmentDirections.actionFavoritesFragmentToFeedFragment()
+                    )
+                    true
+                }
+                R.id.favoritesFragment_menu -> {
+                    findNavController(R.id.nav_host_fragment).navigate(
+                        FeedFragmentDirections.actionFeedFragmentToFavoritesFragment()
+                    )
+                    true
+                }
+                else -> false
+            }
+        }
+        bottomNavigationView.setOnNavigationItemReselectedListener {}
     }
 }
